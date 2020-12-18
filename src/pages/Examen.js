e57e66788d6679756904bf572aff216590eb535f
Leandro Marrero <leandro.marrero@outlook.com>
2020-12-18 14:54:43 -0300

    Segundo commit

--- src/pages/Examen.js
+++ src/pages/Examen.js
@@ -20,7 +20,8 @@ const Maira = new USUARIO(26372734, 'f', 'Maira');
 const Leandro = new USUARIO(49189815, 'm', 'Leandro');
 const Arihana = new USUARIO(50379752, 'f', 'Arihana');
 
-const arreglo = [Maira, Leandro, Arihana];
+const Marcos = new USUARIO(51196719, 'm', 'Marcos');
+const arreglo = [Maira, Leandro, Arihana, Marcos];
 
 /*******************************************/
 
