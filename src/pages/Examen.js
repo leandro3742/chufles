13469b06e18b102b15c2686e062616f12b42be6a
Leandro Marrero <leandro.marrero@outlook.com>
2021-02-04 16:00:42 -0300

    sin usuarios

--- src/pages/Examen.js
+++ src/pages/Examen.js
@@ -20,11 +20,7 @@ const Maira = new USUARIO(26372734, 'f', 'Maira');
 const Leandro = new USUARIO(49189815, 'm', 'Leandro');
 const Arihana = new USUARIO(50379752, 'f', 'Arihana');
 
-const Renato = new USUARIO(41803380, 'm', 'Renato');
-const Ana = new USUARIO(53540312, 'f', 'Paula'); //Ana paula Biganzoli
-const Santiago = new USUARIO(54407323, 'm', 'Santiado'); //Santiago Sanchez
-
-const arreglo = [Maira, Leandro, Arihana, Renato, Ana, Santiago];
+const arreglo = [Maira, Leandro, Arihana];
 
 /*******************************************/
 
